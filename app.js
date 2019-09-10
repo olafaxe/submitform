4c5d94cae7046f92c9bf379e358ada8a0e70bf0c
Ola Faxe <olafaxe@gmail.com>
2019-09-10 14:14:38 +0200

    post get mongo

--- app.js
+++ app.js
@@ -12,12 +12,37 @@ app.get("/signup", (req, res) => {
 
 app.post("/signup", (req, res) => {
   const username = req.body.username;
-  //...
+  const usersCollection = db.collection("users");
+
   //   res.render("pages/signup.ejs");
   console.log(req.body.username);
   res.render("pages/signup.ejs", { username });
+  usersCollection.insertOne({ item: username });
 });
 
 app.listen(port, () => {
   console.log(`lyssnar på ${port}`);
+});
+
+const MongoClient = require("mongodb").MongoClient;
+
+// Connection URL
+const url = "mongodb://localhost:27017";
+
+// Create a new MongoClient
+const client = new MongoClient(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+let db;
+
+client.connect(function(err) {
+  if (err) throw err;
+  db = client.db("fake");
+});
+app.get("/users", (req, res) => {
+  const usersCollection = db.collection("users");
+  usersCollection.find({}).toArray(function(err, users) {
+    res.send(users);
+  });
 });
